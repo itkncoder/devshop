7d10c6963dd4865cf2e8820502dc8b1818565c72
KNCoder <kncoderr@gmail.com>
2023-05-23 19:14:20 +0500

    init4

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,57 +1,22 @@
 import {
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalFooter,
-    ModalBody,
-    ModalCloseButton,
     Box,
-    useDisclosure,
     Text,
-    Button,
 } from '@chakra-ui/react'
-import { faAngleRight } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Image from 'next/image'
 import React from 'react'
 
-const Card = () => {
-
-    const { isOpen, onOpen, onClose } = useDisclosure()
-
+const Card = ({img}: {img: string}) => {
     return (
-        <Box mx={"8px"} >
-            <Box p={"20px 15px 20px 15px"} rounded={"2xl"} border={"1px solid rgba(256, 256, 256, 0.1)"} position={"relative"}>
-                <Box>
-                    <Image priority onClick={onOpen} style={{width: "100%", height: "320px", objectFit: "cover", borderRadius: "20px", cursor: "pointer"}} width={500} height={400} alt='product' src={"https://merchshop.in/wp-content/uploads/2019/06/Apple-Developer-t-shirt-white.jpg"} />
+        <Box bg={"#0a0a0a"} p={"20px"} shadow={"2xl"} rounded={"2xl"} mx={"8px"} border={"1px solid rgba(256, 256, 256, 0.1)"} >
+            <Box position={"relative"}>
+                <Box display={"flex"} justifyContent={"center"} >
+                    <Image priority style={{width: "100%", height: "320px", objectFit: "cover", borderRadius: "12px", cursor: "pointer"}} width={500} height={400} alt='product' src={img} />
                 </Box>
-                <Box display={"flex"} justifyContent={"space-between"} alignItems={"center"} p={"5px 5px 5px 5px"} >
-                    <Box cursor={"pointer"} >
-                        <Text color={"#DDE6ED"} fontSize={"lg"} fontWeight={"600"} >Product name</Text>
-                        <Text color={"#DDE6ED"} maxW={"200px"} overflow={"hidden"} whiteSpace={"nowrap"} textOverflow={"ellipsis"} >Product desdfsdfscription about</Text>
-                    </Box>
-                    <Box mr={"5px"}>
-                        <Button variant={"solid"} size={"sm"} _hover={{bg: "#202e42"}} color={"#DDE6ED"} bg={"#27374D"} >
-                            <FontAwesomeIcon icon={faAngleRight} />
-                        </Button>
-                    </Box>
+                <Box display={"flex"} flexDirection={"column"} justifyContent={"center"} alignItems={"center"} p={"5px 5px 5px 5px"} >
+                    <Text color={"#DDE6ED"} textAlign={"center"} fontSize={"18px"} fontWeight={"600"} >Product name</Text>
+                    <Text color={"#DDE6ED"} textAlign={"center"} maxW={"200px"} overflow={"hidden"} whiteSpace={"nowrap"} textOverflow={"ellipsis"} >$33</Text>
                 </Box>
-                <Text bg={"#385b75"} p={"3px 10px"} color={"#DDE6ED"} rounded={"lg"} position={"absolute"} top={"28px"} right={"25px"} >100$</Text>
             </Box>
-
-
-            <Modal size={"2xl"} isOpen={isOpen} onClose={onClose}>
-                <ModalOverlay />
-                <ModalContent bg={"#27374D"} >
-                    <ModalHeader>Product name</ModalHeader>
-                    <ModalCloseButton />
-                    <ModalBody pb={"30px"} display={"flex"} justifyContent={"center"} alignItems={"center"} >
-                        <Image priority style={{width: "100%", height: "100%", borderRadius: "20px"}} width={1500} height={1500} alt='product' src={"https://www.target.com.au/medias/static_content/product/images/full/62/32/A1766232.jpg?impolicy=product_portrait_hero"} />
-                    </ModalBody>
-                </ModalContent>
-            </Modal>
-
         </Box>
     )
 }
--- src/components/category/category.tsx
+++ src/components/category/category.tsx
@@ -18,35 +18,35 @@ const Category = ({title, desc}: {title: string, desc: JSX.Element}) => {
                 </Box>
             </Box>
             <Box my={"30px"}>
-                <Swiper slidesPerView={4} navigation={true} modules={[Navigation]}>
+                <Swiper slidesPerView={4.2} navigation={true} modules={[Navigation]}>
                     <SwiperSlide>
                         <Box>
-                            <Card/>
+                            <Card img="https://merchshop.in/wp-content/uploads/2019/06/Apple-Developer-t-shirt-white.jpg" />
                         </Box>
                     </SwiperSlide>
                     <SwiperSlide>
                         <Box>
-                            <Card/>
+                            <Card img="https://cdn.shopify.com/s/files/1/0604/4064/1775/products/t-shirt-mockup-of-a-bearded-man-posing-in-front-of-a-patterned-wall-m4201-r-el2.png?v=1663221938" />
                         </Box>
                     </SwiperSlide>
                     <SwiperSlide>
                         <Box>
-                            <Card/>
+                            <Card img='https://images.teemill.com/ypbcmqvqg1zjwipnlkl1do3l9uvjgf4egvau3tkd6h3ns9on.png.jpg?w=1080&h=auto' />
                         </Box>
                     </SwiperSlide>
                     <SwiperSlide>
                         <Box>
-                            <Card/>
+                            <Card img='https://www.swagshirts99.com/wp-content/uploads/2017/06/IMG_5247.jpg' />
                         </Box>
                     </SwiperSlide>
                     <SwiperSlide>
                         <Box>
-                            <Card/>
+                            <Card img='https://www.teez.in/cdn/shop/products/Google-Developer-T-Shirt-1_1024x1024.jpg?v=1589184968' />
                         </Box>
                     </SwiperSlide>
                     <SwiperSlide>
                         <Box>
-                            <Card/>
+                            <Card img='https://www.getdigital.eu/images/productsMore/__generated__resized/1100x1100/20156false_false_model.jpg' />
                         </Box>
                     </SwiperSlide>
                 </Swiper>
